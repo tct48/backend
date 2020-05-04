4d2a871b3cbd82c27e15949db73f8a12ec5a9fa8
tct48 <tar_solo@outlook.co.th>
2020-05-04 21:18:11 +0700

    commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
