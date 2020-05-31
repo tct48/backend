a485eded1f5598a022ce2959fa45d2841d017bda
tct48 <tar_solo@outlook.co.th>
2020-05-31 18:23:33 +0700

    edit class room select exp

--- api/routes/user/classroom.js
+++ api/routes/user/classroom.js
@@ -66,7 +66,7 @@ router.get("/student/:classroom", (req, res, next) => {
   const user = User.find({
     class: classroom,
   })
-  .select("firstname lastname phone")
+  .select("firstname lastname phone exp")
   user.then((result) => {
     return res.status(200).json({
       total_items: result.length,
