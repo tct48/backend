8bb791870052e76ad98ac1876358f07b989829c9
ketarWorapol <s6102041520131@email.kmutnb.ac.th>
2020-06-08 10:20:52 +0700

    let go push wtf

--- api/routes/quiz/quiz.js
+++ api/routes/quiz/quiz.js
@@ -7,7 +7,7 @@ const Quiz = require("../../models/quiz/quiz");
 // เรียกดูข้อ ทั้งหมด ของ Chapter นั้น ๆ
 router.get("/:_id", (req, res, next) => {
   const _id = req.params._id;
-  Quiz.findOne({ _id: _id }).then((result) => {
+  Quiz.findOne({ ref: _id }).then((result) => {
     return res.status(200).json({
       total_items: result.choice.length,
       items: result,
